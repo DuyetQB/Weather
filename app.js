36f0f356c575d290a942d954da3cc2cffaa8c7d1
DuyetQB <dinhsyduyet@gmail.com>
2021-02-14 19:48:34 +0700

    commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// Tutorial by http://youtube.com/CodeExplained
-// api key : 82005d27a116c2880c8f0fcb866998a0
+//dinh si duyet
+// api key : 7a32b63391ffce63e0a3ed6b4193e225
 
 // SELECT ELEMENTS
 const iconElement = document.querySelector(".weather-icon");
